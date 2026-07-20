9d9072d361e46321d55107660c4236fac4f97383
Fujita <manabu.fjt@gmail.com>
2026-07-20 22:04:02 +0900

    リファクタリング（ヘッダー/ナビ/カラー編集/サンプル/パターン/フッターをセクション単位で分離）

---
+++ app/components/NavMenu.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Offcanvas, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+type NavMenuProps = {
+  show: boolean;
+  toggleShow: () => void;
+};
+
+/** 画面右からスライドインするナビゲーションメニュー(Home/About/Contact)。 */
+export const NavMenu = ({ show, toggleShow }: NavMenuProps) => {
+  return (
+    <Offcanvas show={show} onHide={toggleShow} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>My SPA Site</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Nav className="flex-column">
+          <Nav.Link as={Link} to="/*" onClick={toggleShow}>
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about" onClick={toggleShow}>
+            About
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact" onClick={toggleShow}>
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Offcanvas.Body>
+    </Offcanvas>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,19 @@
 "use client";
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 
-
-import { getCurrentColor,
-  handleToggleColorButton,
-  getColorButtonStyle} from './components/ColorComponents';
-import { addCard, removeCard } from './components/CardComponents';
 import { useColorEditor } from './components/useColorEditor';
-
-
-import { useState } from 'react';
+import { DivColor } from './components/DivColor';
+import { PageHeader } from './components/PageHeader';
+import { NavMenu } from './components/NavMenu';
+import { ColorEditorSection } from './components/ColorEditorSection';
+import { SampleColorSection } from './components/SampleColorSection';
+import { PatternSection } from './components/PatternSection';
+import { PageFooter } from './components/PageFooter';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
-import { Container, Navbar, Nav, Offcanvas, Button, Row, Col, ButtonToolbar, ButtonGroup } from 'react-bootstrap';
-
-// import { get } from 'http';
-// import InputGroupText from 'react-bootstrap/esm/InputGroupText';
-
-import { faBrush } from "@fortawesome/free-solid-svg-icons";
-import { ColorSpacePanel, colorSpaceConfigs } from './components/ColorSpacePanel';
-import { DivColor } from './components/DivColor';
-import { DivCard } from './components/DivCard';
-import { DivSampleColors } from './components/SampleColorList';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Wrapper = dynamic(() => import('./components/Wrapper'), { ssr: false });
 
@@ -45,71 +34,25 @@ function App() {
     appRef, headerRef, bodyRef, footerRef,
     resetPage,
     handleChangeRange,
-    changeClickedColor,
     addComponent,
     getColorStyle,
     handleClick,
-    handleClickClear,
-    handleHover,
     handleDragStart,
     handleDragOver,
     handleDrop,
   } = useColorEditor();
 
-  // const [appHeight, setAppHeight] = useState<number>(document.documentElement.clientHeight);
-
   // ページロード時の処理
   useEffect(() => {
     console.log('load page')
 
-    // setComponents(getInitialComponents());
-
     const onResize = () => {
       // 画面の高さを再計算
-      // setAppHeight(window.innerHeight);
-      // setAppHeight(document.documentElement.clientHeight);
-      // console.log('appHeight:' + appHeight)
     }
     window.addEventListener('resize', onResize);
     return () => window.removeEventListener('resize', onResize);
 
-
   }, []);
-
-  // useEffect(() => {
-  //   // 選択箇所の色、サンプルも変更する
-  //   changeClickedColor();
-
-  //   // localStrageに保存
-  //   localStorage.setItem('currentColors', JSON.stringify(currentColors));
-  // }, [currentColors]);
-
-  // 表示・非表示機能
-  const [showColorSelector, setShowColorSelector] = useState<boolean>(true);
-  const [showSampleColor, setShowSampleColor] = useState<boolean>(true);
-
-  const handleToggleColorSelector = () => {
-    setShowColorSelector(!showColorSelector);
-  }
-
-  const getShowAllColorSelector = () => {
-    return showColorSelector ? '' : {style: {display: 'none' }}
-  }
-
-  const handleToggleSampleColor = () => {
-    setShowSampleColor(!showSampleColor);
-  }
-
-  const getShowSampleColor = () => {
-    return showSampleColor ? '' : {style: {display: 'none' }}
-  }
-
-
-  // 各色の表示・非表示
-  const [showColorRGB, setShowColorRGB] = useState<boolean>(true);
-  const [showColorHSV, setShowColorHSV] = useState<boolean>(true);
-  const [showColorHSL, setShowColorHSL] = useState<boolean>(true);
-  const [showColorCMYK, setShowColorCMYK] = useState<boolean>(true);
 
   return (
     <>
@@ -117,174 +60,44 @@ function App() {
 
     <div className="App" ref={appRef}>
 
+      <PageHeader headerRef={headerRef} resetPage={resetPage}
+        addComponent={addComponent} getColorStyle={getColorStyle}
+        handleClick={handleClick} handleDragStart={handleDragStart} handleDragOver={handleDragOver} handleDrop={handleDrop} />
 
-      {/* <BrowserRouter> */}
+      <NavMenu show={show} toggleShow={toggleShow} />
 
-        {/* ヘッダー */}
-        <Navbar bg="dark" variant="dark" expand="lg" fixed="top" id="header2" ref={headerRef}>
-            <DivColor id='header' className="header pt-2 navbar-brand "
+      {/* body用の背景 */}
+      <DivColor id='body-background'
+        addComponent={addComponent} getColorStyle={getColorStyle}
+        handleClick={handleClick} handleDragStart={handleDragStart} handleDragOver={handleDragOver} handleDrop={handleDrop}></DivColor>
+
+      {/* Body */}
+      <Container className='mt-5 pt-5 body-main' ref={bodyRef}>
+        <Row>
+          {/* Col */}
+          <Col xs={12} md={9}>
+            <ColorEditorSection components={components} handleChangeRange={handleChangeRange} />
+          </Col>
+
+          {/* Col */}
+          <Col xs={12} md={3}>
+            <SampleColorSection components={components} setComponents={setComponents}
               addComponent={addComponent} getColorStyle={getColorStyle}
-              handleClick={handleClick} handleDragStart={handleDragStart} handleDragOver={handleDragOver} handleDrop={handleDrop}>
-              <Container>
-                <Row>
-                  <Col sm={2}>
-                    <Navbar.Brand>{ process.env.NEXT_PUBLIC_REACT_APP_TITLE }</Navbar.Brand>
-                  </Col>
-                  <Col className="ms-auto"></Col>
-                  <Col sm={1}>
-                    {/* localStorageのクリアボタンを右寄せで表示 */}
-                    <Button variant="outline-danger" className="float-right" onClick={() => {resetPage()}}>Reset</Button>
-                  </Col>
-                </Row>
-              </Container>
-            </DivColor>
-        </Navbar>
+              handleClick={handleClick} handleDragStart={handleDragStart} />
 
+            <PatternSection cardCount={cardCount} setCardCount={setCardCount}
+              addComponent={addComponent} getColorStyle={getColorStyle}
+              handleClick={handleClick} handleDragStart={handleDragStart} handleDragOver={handleDragOver} handleDrop={handleDrop} />
+          </Col>
+        </Row>
+      </Container>
 
+      {/* フッター */}
+      <Row className="m-5"/>
 
-        <Offcanvas show={show} onHide={toggleShow} placement="end">
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>My SPA Site</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="flex-column">
-              <Nav.Link as={Link} to="/*" onClick={toggleShow}>
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/about" onClick={toggleShow}>
-                About
-              </Nav.Link>
-              <Nav.Link as={Link} to="/contact" onClick={toggleShow}>
-                Contact
-              </Nav.Link>
-            </Nav>
-          </Offcanvas.Body>
-        </Offcanvas>
-
-      
-        {/* body用の背景 */}
-        <DivColor id='body-background'
-          addComponent={addComponent} getColorStyle={getColorStyle}
-          handleClick={handleClick} handleDragStart={handleDragStart} handleDragOver={handleDragOver} handleDrop={handleDrop}></DivColor>
-
-
-        {/* Body */}
-        <Container className='mt-5 pt-5 body-main' ref={bodyRef}>
-          <Row>
-            {/* Col */}
-            <Col xs={12} md={9}>
-
-            {/* 設定ボタン */}
-            <Row>
-              
-            </Row>
-
-
-
-            {/* カラーバー */}
-            <ButtonToolbar aria-label="Toolbar with button groups" className='mt-2'>
-              <ButtonGroup className="me-2" aria-label="First group">
-                <Button variant="outline-secondary" size="sm" onClick={() => {handleToggleColorSelector()}}>{showColorSelector ? "hide" : "show"}</Button>
-              </ButtonGroup>
-              <ButtonGroup className="me-2" aria-label="Second group"　{...getShowAllColorSelector()}>
-                <Button variant="outline-primary" size="sm" className="hide-button" onClick={() => {handleToggleColorButton('button_rgb', showColorRGB, showColorHSV, showColorHSL, showColorCMYK, setShowColorRGB, setShowColorHSV, setShowColorHSL, setShowColorCMYK)}} {...getColorButtonStyle('button_rgb', showColorRGB, showColorHSV, showColorHSL, showColorCMYK)}>RGB</Button>
-                <Button variant="outline-primary" size="sm" className="hide-button" onClick={() => {handleToggleColorButton('button_hsv', showColorRGB, showColorHSV, showColorHSL, showColorCMYK, setShowColorRGB, setShowColorHSV, setShowColorHSL, setShowColorCMYK)}} {...getColorButtonStyle('button_hsv', showColorRGB, showColorHSV, showColorHSL, showColorCMYK)}>HSV</Button>
-                <Button variant="outline-primary" size="sm" className="hide-button" onClick={() => {handleToggleColorButton('button_hsl', showColorRGB, showColorHSV, showColorHSL, showColorCMYK, setShowColorRGB, setShowColorHSV, setShowColorHSL, setShowColorCMYK)}} {...getColorButtonStyle('button_hsl', showColorRGB, showColorHSV, showColorHSL, showColorCMYK)}>HSL</Button>
-                <Button variant="outline-primary" size="sm" className="hide-button" onClick={() => {handleToggleColorButton('button_cmyk', showColorRGB, showColorHSV, showColorHSL, showColorCMYK, setShowColorRGB, setShowColorHSV, setShowColorHSL, setShowColorCMYK)}} {...getColorButtonStyle('button_cmyk', showColorRGB, showColorHSV, showColorHSL, showColorCMYK)}>CMYK</Button>
-              </ButtonGroup>
-            </ButtonToolbar>
-
-            <Container className='border mt-3 color-bars' fluid="md">
-
-              {/* <Button variant="outline-secondary" size="sm" onClick={() => {handleToggleColorSelector()}}>{showColorSelector ? "隠す" : "表示"}</Button> */}
-              <Row {...getShowAllColorSelector()}>
-
-                {/* 色変更バー(RGB/HSV/HSL/CMYK) */}
-                {colorSpaceConfigs.map((config) => (
-                  <ColorSpacePanel
-                    key={config.id}
-                    config={config}
-                    color={getCurrentColor(components)}
-                    handleChangeRange={handleChangeRange}
-                    showColorRGB={showColorRGB}
-                    showColorHSV={showColorHSV}
-                    showColorHSL={showColorHSL}
-                    showColorCMYK={showColorCMYK}
-                  />
-                ))}
-
-              </Row>
-            </Container>
-
-
-
-
-            </Col>
-
-            {/* Col */}
-            <Col xs={12} md={3}>
-
-              
-              {/* サンプルカラー */}
-              <ButtonToolbar aria-label="Toolbar with button groups" className='mt-4'>
-                <ButtonGroup className="me-2" aria-label="First group">
-                  <Button variant="outline-secondary" size="sm" onClick={() => {handleToggleSampleColor()}}>{showSampleColor ? "hide" : "show"}</Button>
-                </ButtonGroup>
-              </ButtonToolbar>
-
-              <Container className='border mt-3'>
-                <Row {...getShowSampleColor()}>
-                  <DivSampleColors
-                    components={components} setComponents={setComponents}
-                    addComponent={addComponent} getColorStyle={getColorStyle}
-                    handleClick={handleClick} handleDragStart={handleDragStart} />
-                </Row>
-              </Container>
-
-              {/* Pattern */}
-              <Row className="mt-5" id="card" >
-                <Col sm={12}>
-                  <Button variant="outline-secondary" size="sm" className='button-card' onClick={() => removeCard(cardCount, setCardCount)}>-</Button>
-                  <span className='mx-2'>Pattern</span>
-                  <Button variant="outline-secondary" size="sm" className='button-card' onClick={() => addCard(cardCount, setCardCount)}>+</Button>
-                </Col>
-                <DivCard cardCount={cardCount}
-                  addComponent={addComponent} getColorStyle={getColorStyle}
-                  handleClick={handleClick} handleDragStart={handleDragStart} handleDragOver={handleDragOver} handleDrop={handleDrop} />
-              </Row>
-
-            </Col>
-          </Row>
-        </Container>
-
-        {/* フッター */}
-        <Row className="m-5"/>
-
-        <Navbar expand="lg" fixed="bottom" className='footer' ref={footerRef}>
-          
-
-            {/* <Navbar.Collapse className="justify-content-center"> */}
-              {/* <Navbar.Text> */}
-                <DivColor id='footer'
-                  addComponent={addComponent} getColorStyle={getColorStyle}
-                  handleClick={handleClick} handleDragStart={handleDragStart} handleDragOver={handleDragOver} handleDrop={handleDrop}>
-                  <Container>
-                    <Row>
-                      <Col className="ms-auto"></Col>
-                      <Col md={4}>
-                        <span>&copy; 2024 Design Palette. All rights reserved.</span>
-                      </Col>
-                      <Col className="ms-auto"></Col>
-                    </Row>
-                  </Container>
-                </DivColor>
-              {/* </Navbar.Text> */}
-            {/* </Navbar.Collapse> */}
-
-
-        </Navbar>
-        
-      {/* </BrowserRouter> */}
+      <PageFooter footerRef={footerRef}
+        addComponent={addComponent} getColorStyle={getColorStyle}
+        handleClick={handleClick} handleDragStart={handleDragStart} handleDragOver={handleDragOver} handleDrop={handleDrop} />
 
     </div>
 
